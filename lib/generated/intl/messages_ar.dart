1effc20d88603db0ea320191987d34d2b13c7205
Leena Almatar <LeenaAlmatar12@gmail.com>
2024-03-24 00:27:22 +0300

    "translation"

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,164 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage(
+            "في سقيا نحن متحمسون لتحسين تجربتك في المسجد الحرام."),
+        "aboutSuqia": MessageLookupByLibrary.simpleMessage(
+            "سنساعدك في الوصول إلى حافظات مياه زمزم باستخدام إعدادات الاتجاه الخاصة بك لإرشادك إلى أقرب خزان مياه زمزم بناءً على مواصفاتك بأعلى جودة وأسهل طريقة."),
+        "aboutUs": MessageLookupByLibrary.simpleMessage("عن سقيا"),
+        "checkEmail": MessageLookupByLibrary.simpleMessage(
+            "يرجى التحقق من بريدك الإلكتروني وإدخال الرمز المرسل للتحقق من هويتك لتتمكن من تغيير كلمة المرور الخاصة بك."),
+        "codeTime": MessageLookupByLibrary.simpleMessage(
+            "تنتهي صلاحية الرمز بعد دقيقتين"),
+        "cold": MessageLookupByLibrary.simpleMessage("باردة"),
+        "contactUs": MessageLookupByLibrary.simpleMessage("تواصل معنا"),
+        "dataReten":
+            MessageLookupByLibrary.simpleMessage("الاحتفاظ بالبيانات:"),
+        "dataRetention": MessageLookupByLibrary.simpleMessage(
+            "نحن نحتفظ بمعلومات موقعك طالما كان ذلك ضروريًا لتحقيق الأغراض الموضحة في هذه السياسة ما لم تكن فترة الاحتفاظ الأطول مطلوبة أو مسموح بها بموجب القانون."),
+        "distance": MessageLookupByLibrary.simpleMessage("مسافة"),
+        "email": MessageLookupByLibrary.simpleMessage("بريد الإلكتروني"),
+        "emailCode": MessageLookupByLibrary.simpleMessage(
+            "رجاء تحقق البريد الالكتروني وادخال الرمز المرسل للتحقق من بريد الالكتروني"),
+        "emailEmpty": MessageLookupByLibrary.simpleMessage(
+            "رجاء قم بتعبئة بريدك الإلكتروني"),
+        "emailErrorMes": MessageLookupByLibrary.simpleMessage(
+            "بريد الألكتروني يجب أن ينتهي ب @suqia.com"),
+        "emailMessage":
+            MessageLookupByLibrary.simpleMessage("ادخل بريدك الإلكتروني"),
+        "employee": MessageLookupByLibrary.simpleMessage("موظف سقيا"),
+        "empty": MessageLookupByLibrary.simpleMessage("فارغة"),
+        "enquirers": MessageLookupByLibrary.simpleMessage("للاستفسار والدعم:"),
+        "firstName": MessageLookupByLibrary.simpleMessage("الأسم الأول"),
+        "firstNameEmpty":
+            MessageLookupByLibrary.simpleMessage("رجاء قم بتعبئة اسمك الأول"),
+        "firstNameMessage":
+            MessageLookupByLibrary.simpleMessage("ادخل اسمك الأول"),
+        "floor": MessageLookupByLibrary.simpleMessage("الدور"),
+        "full": MessageLookupByLibrary.simpleMessage("ممتلئة"),
+        "haveAcc": MessageLookupByLibrary.simpleMessage("هل لديك حساب؟ "),
+        "infoCollect": MessageLookupByLibrary.simpleMessage("جمع المعلومات:"),
+        "infoCollection": MessageLookupByLibrary.simpleMessage(
+            "عندما تستخدم تطبيق سثيا ، قد نقوم بجمع معلومات معينة، مثل المعرف الفريد لجهازك، وبيانات الموقع، وإحصائيات استخدام التطبيق."),
+        "infoShar": MessageLookupByLibrary.simpleMessage("مشاركة المعلومات:"),
+        "infoSharege": MessageLookupByLibrary.simpleMessage(
+            "نحن لا نشارك معلوماتك الشخصية مع أطراف ثالثة لأغراض التسويق أو إي هدف أخر."),
+        "infoUpdate":
+            MessageLookupByLibrary.simpleMessage("تم تحديث البيانات بنجاح"),
+        "infoUsage": MessageLookupByLibrary.simpleMessage(
+            "نحن نستخدم المعلومات التي نجمعها لتحسين وظائف تطبيق سقيا وتزويدك بتجربة مخصصة."),
+        "infoUse": MessageLookupByLibrary.simpleMessage("استخدام المعلومات:"),
+        "intro": MessageLookupByLibrary.simpleMessage("المقدمة:"),
+        "introPri": MessageLookupByLibrary.simpleMessage(
+            "تنطبق سياسة الخصوصية هذه على تطبيق الهاتف المحمول الخاص بنا، وهو يشرح كيف نقوم بجمع واستخدام وحماية معلوماتك الشخصية عند استخدامك سقيا."),
+        "lanAr": MessageLookupByLibrary.simpleMessage("اللغة العربية"),
+        "lanChange": MessageLookupByLibrary.simpleMessage("غير اللغة"),
+        "lanEn": MessageLookupByLibrary.simpleMessage("اللغة الإنجيلزية"),
+        "lanSelect": MessageLookupByLibrary.simpleMessage("اختر لغة"),
+        "langMessg":
+            MessageLookupByLibrary.simpleMessage("تم تغير اللغة بنجاح"),
+        "language": MessageLookupByLibrary.simpleMessage("اللغة"),
+        "lastName": MessageLookupByLibrary.simpleMessage("الأسم الأخير"),
+        "lastNameEmpty":
+            MessageLookupByLibrary.simpleMessage("رجاء قم بتعبئة اسمك الأخير"),
+        "lastNameMessage":
+            MessageLookupByLibrary.simpleMessage("ادخل اسمك الأخير"),
+        "lastUpd":
+            MessageLookupByLibrary.simpleMessage("آخر تحديث: 2023/09/25"),
+        "locate": MessageLookupByLibrary.simpleMessage("حدد أو ارشدني"),
+        "logInButton": MessageLookupByLibrary.simpleMessage("تسجيل دخول"),
+        "logOut": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+        "logOutMes":
+            MessageLookupByLibrary.simpleMessage("تم تسجيل الخروج بنجاح"),
+        "loginError": MessageLookupByLibrary.simpleMessage(
+            "بريد إلكتروني أو الرمز السري غير صحيحين"),
+        "loginPage": MessageLookupByLibrary.simpleMessage("تسجيل الدخول"),
+        "mapTittle": MessageLookupByLibrary.simpleMessage("الخريطة"),
+        "newPass": MessageLookupByLibrary.simpleMessage("الرمز السري الجديد"),
+        "noAccount": MessageLookupByLibrary.simpleMessage("هل لا تملك حساب ؟"),
+        "ok": MessageLookupByLibrary.simpleMessage("موافق"),
+        "passChar": MessageLookupByLibrary.simpleMessage(
+            "يجب أن تحتوي كلمة المرور على حرف خاص واحد على الأقل"),
+        "passConf": MessageLookupByLibrary.simpleMessage("تأكيد الرمز السري"),
+        "passConfHint":
+            MessageLookupByLibrary.simpleMessage("رجاء اعد كتابة الرمز السري"),
+        "passDig": MessageLookupByLibrary.simpleMessage(
+            "يجب أن تحتوي كلمة المرور على رقم واحد على الأقل"),
+        "passEmpty":
+            MessageLookupByLibrary.simpleMessage("رجاء قم بإدخال رمز سري"),
+        "passForget": MessageLookupByLibrary.simpleMessage("نسيت الرمز السري؟"),
+        "passLow": MessageLookupByLibrary.simpleMessage(
+            "يجب أن تحتوي كلمة المرور على حرف صغير واحد على الأقل"),
+        "passMessage": MessageLookupByLibrary.simpleMessage("ادخل رمز السري"),
+        "passNotMatch":
+            MessageLookupByLibrary.simpleMessage("الرمز السري لا يتطابق"),
+        "passRest":
+            MessageLookupByLibrary.simpleMessage("إعادة تعيين الرمز السري"),
+        "passShort": MessageLookupByLibrary.simpleMessage(
+            "يجب أن يكون الرمز السري أكثر من ٦ رموز"),
+        "passUpp": MessageLookupByLibrary.simpleMessage(
+            "يجب أن تحتوي كلمة المرور على حرف كبير واحد على الأقل"),
+        "password": MessageLookupByLibrary.simpleMessage("رمز السري"),
+        "phoneNum": MessageLookupByLibrary.simpleMessage("رقم الجوال"),
+        "priTittle":
+            MessageLookupByLibrary.simpleMessage("سياسة الخصوصية لسقيا"),
+        "privacy": MessageLookupByLibrary.simpleMessage("خصوصية والسرية"),
+        "profile": MessageLookupByLibrary.simpleMessage("ملف الشخصي"),
+        "reNewPass": MessageLookupByLibrary.simpleMessage(
+            "أعد كتابة الرمز السري الجديد"),
+        "refillCount":
+            MessageLookupByLibrary.simpleMessage("عدد مرات إعادة التعبئة"),
+        "rememberMe": MessageLookupByLibrary.simpleMessage("تذكرني"),
+        "resendCode": MessageLookupByLibrary.simpleMessage("أعادة أرسال الرمز"),
+        "reset": MessageLookupByLibrary.simpleMessage("اعد تعيين"),
+        "resetPassSuc": MessageLookupByLibrary.simpleMessage(
+            "تم إعادة تعين الرمز السري بنجاح"),
+        "resetPassTittle":
+            MessageLookupByLibrary.simpleMessage("إعادة تعين الرمز السري"),
+        "save": MessageLookupByLibrary.simpleMessage("حفظ"),
+        "send": MessageLookupByLibrary.simpleMessage("إرسال"),
+        "sendEmail": MessageLookupByLibrary.simpleMessage(
+            "الرجاء إدخال بريدك الإلكتروني لنرسل لك الرمز لبدء عملية إعادة تعيين كلمة المرور الخاصة بك."),
+        "setTitle": MessageLookupByLibrary.simpleMessage("الأعدادت"),
+        "signUpButton": MessageLookupByLibrary.simpleMessage("التسجيل"),
+        "signUpTitle": MessageLookupByLibrary.simpleMessage("Sign Up"),
+        "tInfoTitle":
+            MessageLookupByLibrary.simpleMessage("معلومات حافظة الماء"),
+        "tListTitle":
+            MessageLookupByLibrary.simpleMessage("قائمة حافظات مياة زمزم"),
+        "tStatus": MessageLookupByLibrary.simpleMessage("حالة حافظة الماء"),
+        "tank": MessageLookupByLibrary.simpleMessage("حافظة ماء"),
+        "tankNum": MessageLookupByLibrary.simpleMessage("رقم الحافظة"),
+        "temperature": MessageLookupByLibrary.simpleMessage("درجة الحرارة"),
+        "verifIdn": MessageLookupByLibrary.simpleMessage("تحقق من هويتك"),
+        "verifPageTitle": MessageLookupByLibrary.simpleMessage("تحقق"),
+        "verifyButton": MessageLookupByLibrary.simpleMessage("تحقق"),
+        "verifyEmail":
+            MessageLookupByLibrary.simpleMessage("تحقق من بريدك الالكتروني"),
+        "visitor": MessageLookupByLibrary.simpleMessage("زائر"),
+        "welcome": MessageLookupByLibrary.simpleMessage("هلا وسهلاً فيك سقيا"),
+        "welcomeMessage": MessageLookupByLibrary.simpleMessage(
+            "سقيا يساعدك توصل لمياة زمزم بكل سهولة!")
+      };
+}
